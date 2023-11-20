b9d79a36dc6eea75c16d4b0c8456f62a01c5dd21
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-11-21 01:21:18 +0530

    Added Routes and Video card

---
+++ client/src/components/Card.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 342px;
+    margin-bottom: 25px;
+    cursor: pointer;
+
+`;
+const Image = styled.img`
+    width: 100%;
+    height: 192px;  
+    cursor: pointer;
+    border-radius:10px;
+`;
+const Texts = styled.div`
+
+`;
+const ChannelName = styled.h2`
+    font-size:14px;
+    font-weight:400;
+    color: ${({theme})=>theme.textSoft};
+    margin: 8px 0px;
+`;
+const Title = styled.h1`
+    font-size:16px;
+    font-weight:500;
+    color: ${({theme})=>theme.text};
+`;
+const Info = styled.h2`
+    font-size:14px;
+    font-weight:400;
+    color: ${({theme})=>theme.textSoft};
+`;
+const Details = styled.div`
+    display:flex;
+    margin-top:8px;
+    gap:12px;
+`;
+const ChannelImage = styled.img`
+    height:36px;
+    width:36px;
+    border-radius:50%;
+    cursor: pointer;
+`;
+const Card = () => {
+  return (
+    <Container>
+      <Image src="https://i.ytimg.com/vi/HeZO_LcQALI/maxresdefault.jpg"/>
+      <Details>
+        <ChannelImage src="https://mir-s3-cdn-cf.behance.net/projects/404/a2c2a9181643811.Y3JvcCwyMzAxLDE4MDAsMjUwLDA.jpg"/>
+        <Texts>
+            <Title>Title</Title>
+            <ChannelName>Channel name</ChannelName>
+            <Info>Info</Info>
+        </Texts>
+      </Details>
+    </Container>
+  )
+}
+
+export default Card
